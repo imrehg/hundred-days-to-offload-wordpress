88fb15a20f519f980d59ddac4116d022582db5b7
Gergely Imreh <gergely@imreh.net>
2022-07-04 10:49:43 +0800

    code quality changes and all that

--- hundred-days-to-offload.php
+++ hundred-days-to-offload.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name:       Hundred Days to Offload
  * Plugin URI:        https://github.com/imrehg/hundred-days-to-offload-wordpress
@@ -10,88 +9,103 @@
  * Author URI:        https://gergely.imreh.net
  * License:           apache2
  * License URI:       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @package  HundredDaysToOffload
+ * @category Core
+ * @author   imrehg
+ * @version  0.1.0
  */
 
-function hudred_days_posts_page_html(): void
-{
-    // check user capabilities
-    if (! current_user_can('publish_posts')) {
-        return;
-    }
+/**
+ * Generate the web page content for this plugin
+ *
+ * @return void
+ */
+function hudred_days_posts_page_html(): void {
+	// Check user capabilities.
+	if ( ! current_user_can( 'publish_posts' ) ) {
+		return;
+	}
 
-    $date_format = "Y-m-d";
-    # Calculate relevant time intervals
-    $cutoff_date = date($date_format, strtotime("-1 year"));
-    #TODO: today's date might not be needed, as post publish days are only in the past?
-    $today_date = date($date_format, getdate()[0]);
+	$date_format = 'Y-m-d';
+	// Calculate relevant time intervals.
+	$cutoff_date = gmdate( $date_format, strtotime( '-1 year' ) );
+	// TODO: today's date might not be needed, as post publish days are only in the past?
+	$today_date = gmdate( $date_format, getdate()[0] );
 
-    $target_count = 100;
+	$target_count = 100;
 
-    $args = array(
-        'post_type'   => 'post',
-        'post_status' => 'publish',
-        'date_query' => [
-            [
-                'after' => $cutoff_date,
-                'before' => $today_date,
-                'inclusive' => true,
-            ]
-        ],
-        'orderby' => 'date',
-        'order' => 'ASC',
-        'nopaging' => true,
-    );
-    $query = new WP_Query($args);
-    $post_count = $query->found_posts;
-    $post_count_text = $post_count . " " . ngettext('post', 'posts', $post_count);
+	$args            = array(
+		'post_type'   => 'post',
+		'post_status' => 'publish',
+		'date_query'  => array(
+			array(
+				'after'     => $cutoff_date,
+				'before'    => $today_date,
+				'inclusive' => true,
+			),
+		),
+		'orderby'     => 'date',
+		'order'       => 'ASC',
+		'nopaging'    => true,
+	);
+	$query           = new WP_Query( $args );
+	$post_count      = $query->found_posts;
+	$post_count_text = $post_count . ' ' . ngettext( 'post', 'posts', $post_count );
 
-    if ($query->have_posts()) {
-        # Get the first post
-        $query->the_post();
-        $have_posts = true;
-        $oldest_date = get_the_date($date_format);
-    } else {
-        $have_posts = false;
-    } ?>
-    <div class="wrap">
-        <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
-        <p>
-        <?php
-            #TODO: This is not correctly translatable just yet
-            echo "Found $post_count_text in the interval from $cutoff_date to today."; ?>
+	if ( $query->have_posts() ) {
+		// Get the first post.
+		$query->the_post();
+		$have_posts  = true;
+		$oldest_date = get_the_date( $date_format );
+	} else {
+		$have_posts = false;
+	} ?>
+	<div class="wrap">
+		<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+		<p>
+		<?php
+			// TODO: This is not correctly translatable just yet.
+			echo esc_html( "Found $post_count_text in the interval from $cutoff_date to today." );
+		?>
 
-        <p>
-        <?php
-        #TODO: This is not correctly translatable just yet
-        if ($post_count >= $target_count) {
-            echo "You've achieved <a href=\"https://100daystooffload.com/\">#100DaysOfOffload!</a> 🎉";
-        } else {
-            $required_count = $target_count - $post_count;
-            echo "Still need to publish $required_count " . ngettext('post', 'posts', $required_count). " ⌨️.";
-        } ?>
-        </p>
+		<p>
+		<?php
+		// TODO: This is not correctly translatable just yet.
+		if ( $post_count >= $target_count ) {
+			echo "You've achieved <a href=\"https://100daystooffload.com/\">#100DaysOfOffload!</a> 🎉";
+		} else {
+			$required_count = $target_count - $post_count;
+			echo esc_html( "Still need to publish $required_count " . ngettext( 'post', 'posts', $required_count ) . ' ⌨️.' );
+		}
+		?>
+		</p>
 
-        <?php if ($have_posts): ?>
-        <p>
-            <?php echo "The oldest post in interval is from $oldest_date." ?>
-        </p>
-        <?php endif; ?>
+		<?php if ( $have_posts ) : ?>
+		<p>
+			<?php echo esc_html( "The oldest post in interval is from $oldest_date." ); ?>
+		</p>
+		<?php endif; ?>
 
 
-    </div>
-    <?php
+	</div>
+	<?php
 }
 
-function hundred_days_options_page(): void
-{
-    add_posts_page(
-        __('Hundred Days to Offload Progress', 'textdomain'),
-        __('Hundred Days to Offload', 'textdomain'),
-        'publish_posts',
-        'hundreddays',
-        'hudred_days_posts_page_html'
-    );
+/**
+ * Add plugin page to the WP interface
+ *
+ * @return void
+ */
+function hundred_days_options_page(): void {
+	add_posts_page(
+		__( 'Hundred Days to Offload Progress', 'textdomain' ),
+		__( 'Hundred Days to Offload', 'textdomain' ),
+		'publish_posts',
+		'hundreddays',
+		'hudred_days_posts_page_html'
+	);
 }
-add_action('admin_menu', 'hundred_days_options_page');
+add_action( 'admin_menu', 'hundred_days_options_page' );
 
 ?>
--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
-== Plugin Name ===
+# Hundred Days to Offload
 Contributors: imrehg
 Tags: 100DaysToOffload
 Requires at least: 6.0
@@ -10,46 +10,46 @@ License URI: https://www.apache.org/licenses/LICENSE-2.0
 
 Check your progress towards 100DaysToOffload, having 100 posts published in the last 1 year.
 
-== Description ==
+## Description
 
 A basic helper to see whether you have achieved [`#100DaysToOffload`](https://100daystooffload.com/),
 that is publishing 100 posts on your website. To see your progress after installing the plugin,
 navigate to **Posts > Hundred Days to Offload** in your WordPress dashboard.
 
-== Frequently Asked Questions ==
+## Frequently Asked Questions
 
-= Can I change the target number of posts? =
+### Can I change the target number of posts?
 
 Not at this point, and probably not in the future either, to keep with the spirit
 of `#100DaysToOffload`.
 
-= Can I change the time interval over which the posts are counted? =
+### Can I change the time interval over which the posts are counted?
 
 Also not at this point, and probably not in the future either, to keep with the spirit
 of `#100DaysToOffload`.
 
-= Can I see whether I achieved 100DaysToOffload in the past? =
+### Can I see whether I achieved 100DaysToOffload in the past?
 
 Not at this point, but looking at adding that feature in the future.
 
-= I have multiple users on my blog, can they have their separate progress? =
+### I have multiple users on my blog, can they have their separate progress?
 
 Not at this point, though possible in the future. The spirit of `#100DaysToOffload`
 is using your personal blog, though, where it's unlikely to have multiple users.
 
-= Should the metric be "100 posts in a year" or "100 unique days when something was posted"? =
+### Should the metric be "100 posts in a year" or "100 unique days when something was posted"?
 
 Yeah, the name and the spirit of the project might be open for multiple interpretations,
 but the [100DaysToOffload website](https://100daystooffload.com/#guidelines) is pretty
 clear on the former interpretation. In the future there might be more options to switch
 the plugin between these two or give you information according to either of them.
 
-== Screenshots ==
+## Screenshots
 
 1. This screen shot description corresponds to screenshot-1.(png|jpg|jpeg|gif). Screenshots are stored in the /assets directory.
 2. This is the second screen shot
 
-== Changelog ==
+## Changelog
 
-= 0.1.0 =
+### 0.1.0
 * Initial release to useful metrics.
